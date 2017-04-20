26f068dc465a0ad1bab91f40dc76fed2faaf58da
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-04-20 15:57:16 +0200

    initial revision sqlite_export.py

---
+++ bin/sqlite_export.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+# coding: utf8
+# -----------------------------------------------------------------
+# export Mediawiki pages in directory structure
+#
+# Copyright (C) 2016-2017, Christophe Fauchard
+# -----------------------------------------------------------------
+
+__version_info__ = (0, 1, 0, 'b1')
+__version__ = '.'.join(map(str, __version_info__))
+
+import argparse
+import sqlite3
+
+parser = argparse.ArgumentParser(description='export Mediawiki pages in directory structure')
+parser.add_argument("--version", action='version', version='%(prog)s ' + __version__)
+parser.add_argument('database', help="database SQLite3 file")
+parser.add_argument('directory', help="export directory")
+
+
+args = parser.parse_args()
+try:
+    conn = sqlite3.connect("file:" + args.database + "?mode=ro", uri=True)
+
+    for page_row in conn.execute('SELECT page_id, page_title FROM page ORDER BY page_title'):
+        page_id = page_row[0]
+        page_name = page_row[1]
+
+        select_newer_revision = "select max(rev_timestamp) from revision where rev_page = %d" % (page_id)
+        select_rev_text_id = "select rev_text_id from revision where rev_timestamp = (%s)" % (select_newer_revision)
+        select_text = "select old_text from text where old_id = (%s)" % (select_rev_text_id)
+
+        for text_row in conn.execute(select_text):
+            fd = open(args.directory + "/" + page_name, mode='w')
+            fd.write(text_row[0])
+            print(page_name)
+            fd.close()
+
+    conn.close()
+
+except sqlite3.OperationalError as error:
+    print(error.__str__())
+    exit(2)
+except FileNotFoundError as error:
+    print(error.strerror, error.filename)
