2e1056fa51d8ba1bb7138c3e8a029b8b46f6d9f9
Christophe Fauchard <christophe.fauchard@gmail.com>
2017-05-07 22:07:51 +0200

    - add .wikitext extension to exported  pages - don't export empty file (images page files) - add .gitignore

--- bin/sqlite_export.py
+++ bin/sqlite_export.py
@@ -6,23 +6,30 @@
 # Copyright (C) 2016-2017, Christophe Fauchard
 # -----------------------------------------------------------------
 
-__version_info__ = (0, 1, 0, 'b1')
+__version_info__ = (0, 2, 0, 'b1')
 __version__ = '.'.join(map(str, __version_info__))
 
 import argparse
 import sqlite3
+import re
 
-parser = argparse.ArgumentParser(description='export Mediawiki pages in directory structure')
-parser.add_argument("--version", action='version', version='%(prog)s ' + __version__)
+parser = argparse.ArgumentParser(
+    description='export Mediawiki pages in directory structure')
+parser.add_argument(
+    "--version",
+    action='version',
+    version='%(prog)s ' + __version__)
 parser.add_argument('database', help="database SQLite3 file")
 parser.add_argument('directory', help="export directory")
 
+re_wikitext = re.compile(".*\..*$")
 
 args = parser.parse_args()
 try:
     conn = sqlite3.connect("file:" + args.database + "?mode=ro", uri=True)
 
-    for page_row in conn.execute('SELECT page_id, page_title FROM page ORDER BY page_title'):
+    for page_row in conn.execute(
+            'SELECT page_id, page_title FROM page ORDER BY page_title'):
         page_id = page_row[0]
         page_name = page_row[1]
 
@@ -31,10 +38,12 @@ try:
         select_text = "select old_text from text where old_id = (%s)" % (select_rev_text_id)
 
         for text_row in conn.execute(select_text):
-            fd = open(args.directory + "/" + page_name, mode='w')
-            fd.write(text_row[0])
-            print(page_name)
-            fd.close()
+            if not re_wikitext.match(page_name):
+                page_name = page_name + ".wikitext"
+                fd = open(args.directory + "/" + page_name, mode='w')
+                fd.write(text_row[0])
+                print(page_name)
+                fd.close()
 
     conn.close()
 
